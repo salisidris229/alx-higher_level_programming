cf5f906f95d5ca21462afd37292f0ada0d78b423
salisidris229 <salisuidris229@gmail.com>
2022-12-05 14:57:13 -0800

    Function that retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > (len(my_list) - 1):
+        return None
+
     else:
-        my_list[idx] = element
-        return my_list
+        return my_list[idx]
