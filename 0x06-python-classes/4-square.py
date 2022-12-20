dc0150672d1a2d4ea688fecaf196fefbc13de00d
salisidris229 <salisuidris229@gmail.com>
2022-12-20 15:59:08 -0800

    task 3 and task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes the size"""
+        self.size = size
+
